39e1a8ae615cb1ff44231850bb01936e05153821
AKintoyeOlamide <akintoyeolamide500@gmail.com>
2020-11-19 08:31:55 +0100

    created the users path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new (user_params) 
+        @user = User.new(user_params) 
         if @user.save
-            redirect_to new_user_path
+            redirect_to user_path(@user)
         else 
             render :new
         end
