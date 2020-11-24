5953a9ff12052e22709ca5dcd9968b77e385fa72
AKintoyeOlamide <akintoyeolamide500@gmail.com>
2020-11-24 10:39:46 +0100

    finising work on the project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all
+    end
+
     def new
        @user = User.new 
     end
 
     def create
-        @user = User.new(user_params) 
+         @user = User.new(user_params) 
+       # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
         if @user.save
-            redirect_to user_path(@user)
+            redirect_to new_user_path
         else 
             render :new
         end
     end
+
+    def edit
+        @user = User.find(params[:id])
+   end
+
+   def update
+       @user = User.find(params[:id])
+       if @user.update(user_params)
+           redirect_to edit_user_path
+       else
+           render :edit
+       end
+   end
     
         private
 
